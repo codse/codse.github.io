e8b91a98f9157a2198faf1b2fba59a0b7f15f192
Hari Lamichhane <onwithhari@gmail.com>
2020-03-26 21:02:14 +0545

    v2 build

---
+++ precache-manifest.459968c067855c1079b9747201d216c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8de0bd903c6cec544b64fca72b66279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26e020b83e68dc073ee3",
+    "url": "/static/css/main.d6a38828.chunk.css"
+  },
+  {
+    "revision": "ad9c83695d100cca6610",
+    "url": "/static/js/2.5311f4b5.chunk.js"
+  },
+  {
+    "revision": "26e020b83e68dc073ee3",
+    "url": "/static/js/main.d2eaae26.chunk.js"
+  },
+  {
+    "revision": "0fe22aefe3825d43c6e4",
+    "url": "/static/js/runtime-main.926f6360.js"
+  },
+  {
+    "revision": "95c46bac018a51878b5e90e39ecc3b22",
+    "url": "/static/media/codse_logo.95c46bac.png"
+  }
+]);
