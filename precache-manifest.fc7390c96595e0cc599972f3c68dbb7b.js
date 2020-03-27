8fa730db74ea4980115467b1776e3ef4e1da1330
Hari Lamichhane <onwithhari@gmail.com>
2020-03-27 12:22:01 +0545

    Fix margin

---
+++ precache-manifest.fc7390c96595e0cc599972f3c68dbb7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee64e5470a282a4bba945e03d4efc25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "354735c557187ecd0190",
+    "url": "/static/css/main.97e007ce.chunk.css"
+  },
+  {
+    "revision": "c3107e7cceb0c55bbbf5",
+    "url": "/static/js/2.1b7c594c.chunk.js"
+  },
+  {
+    "revision": "354735c557187ecd0190",
+    "url": "/static/js/main.b54b4356.chunk.js"
+  },
+  {
+    "revision": "0fe22aefe3825d43c6e4",
+    "url": "/static/js/runtime-main.926f6360.js"
+  },
+  {
+    "revision": "95c46bac018a51878b5e90e39ecc3b22",
+    "url": "/static/media/codse_logo.95c46bac.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e7730548efd0364b26e7ec4d8fcc74a.js"
+  "/precache-manifest.fc7390c96595e0cc599972f3c68dbb7b.js"
 );
 
 self.addEventListener('message', (event) => {
